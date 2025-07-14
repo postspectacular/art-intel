a8c139f984842322b1e776e70d334f05f19972ed
Karsten Schmidt <k@thi.ng>
2025-07-14 13:58:14 +0200

    feat(cli): add analyze-colors cmd

---
+++ packages/cli/src/cmd/analyze-colors.ts
@@ -0,0 +1,86 @@
+import { int, string, type Args, type Command } from "@thi.ng/args";
+import type { AppCtx, CommonOpts } from "../api.js";
+import { ARG_EXT, ARG_IDS, ARG_OUT_DIR } from "../args.js";
+import { analyzeColors, intBufferFromFileARGB } from "@layerinc/analysis";
+import { files, readJSON, writeJSON } from "@thi.ng/file-io";
+import { filter, takeNth } from "@thi.ng/transducers";
+import { illegalArgs } from "@thi.ng/errors";
+import type { Artwork } from "@layerinc/core";
+import { Z2 } from "@thi.ng/strings";
+
+interface AnalyzeColorOpts extends CommonOpts {
+	assetDir: string;
+	ext: string;
+	id: string[];
+	outDir: string;
+	size: number;
+}
+
+export const ANALYZE_COLORS: Command<
+	AnalyzeColorOpts,
+	CommonOpts,
+	AppCtx<AnalyzeColorOpts>
+> = {
+	desc: "Perform color metadata analysis for all or selected artworks",
+	opts: <Args<AnalyzeColorOpts>>{
+		...ARG_EXT,
+		...ARG_IDS,
+		...ARG_OUT_DIR(process.env.LAYER_ANALYSIS_DIR),
+		assetDir: string({
+			desc: "Asset directory containing extracted image sequences. If omitted will use LAYER_TMP_DIR env var",
+			default: process.env.LAYER_TMP_DIR,
+		}),
+		size: int({
+			alias: "s",
+			desc: "Resize image to given size for analysis",
+			default: 256,
+		}),
+	},
+	fn: command,
+};
+
+async function command({ inputs, opts, logger }: AppCtx<AnalyzeColorOpts>) {
+	if (!opts.assetDir) illegalArgs("require --asset-dir");
+	const db = iterateArtworks(
+		readJSON<Artwork[]>(inputs[0] ?? process.env.LAYER_DB_PATH, logger)
+	);
+	const items = opts.id.length
+		? filter((x) => opts.id.includes(x.id), db)
+		: db;
+	for (let item of items) {
+		const frames = takeNth(
+			30,
+			files(opts.assetDir, new RegExp(`${item.id}-\\d{4}.${opts.ext}$`))
+		);
+		let i = 0;
+		for (let frame of frames) {
+			const res = analyzeColors(
+				await intBufferFromFileARGB(frame, opts.size, logger)
+			);
+			writeJSON(
+				`${opts.outDir}/${item.id}-color-${Z2(i++)}.json`,
+				res,
+				undefined,
+				undefined,
+				logger
+			);
+		}
+	}
+}
+
+function* iterateArtworks(db: Artwork[]) {
+	for (let item of db) {
+		yield {
+			id: item.assetID,
+			artworkID: item.artworkID,
+		};
+		if (item.variations) {
+			for (let v of item.variations) {
+				yield {
+					id: v.id,
+					artworkID: item.artworkID,
+				};
+			}
+		}
+	}
+}
